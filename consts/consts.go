b444b79bce30d27c492dfeeba885b6eea9d14b29
royerww <162594984+royerww@users.noreply.github.com>
2024-03-12 19:29:21 +0530

    Update consts.go

--- consts/consts.go
+++ consts/consts.go
@@ -13,11 +13,11 @@ import (
 
 const (
 	// TODO: choose a human-readable part for your hyperchain
-	HRP = ""
+	HRP = "hypersdk"
 	// TODO: choose a name for your hyperchain
-	Name = ""
+	Name = "Metacrafter"
 	// TODO: choose a token symbol
-	Symbol = ""
+	Symbol = "MTS"
 )
 
 var ID ids.ID
